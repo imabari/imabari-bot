9ae7dd9d142923bf2b97babcc2ce61941949028f
imabari <32343845+imabari@users.noreply.github.com>
2023-01-15 00:02:29 +0900

    Update dam_bot.py

--- dam_bot.py
+++ dam_bot.py
@@ -1,98 +1,122 @@
-import datetime
+# -*- coding: utf-8 -*-
+
 import os
-import re
 
 import pandas as pd
-import requests
+import plotly.express as px
+import plotly.figure_factory as ff
 import tweepy
 
-dam = {
-    "name": "玉川ダム",
-    "twt_id": "BotTamagawaDam",
-    "dam_id": "U1001_MMENU003",
-    "CK": os.environ["IMABARI_CK"],
-    "CS": os.environ["IMABARI_CS"],
-    "AT": os.environ["IMABARI_AT"],
-    "AS": os.environ["IMABARI_AS"],
-}
+url = "http://183.176.244.72/kawabou-mng/customizeMyMenuKeika.do?GID=05-5101&userId=U1001&myMenuId=U1001_MMENU003&PG=1&KTM=1"
 
-def date_parse(se, year):
+# 現在
+dt_now = pd.Timestamp.now(tz="Asia/Tokyo").tz_localize(None)
+dt_now
 
-    df = se.str.extract("(\d{2}/\d{2})? *(\d{2}:\d{2})").fillna(method="ffill")
-
-    df_date = (
-        df[0]
-        .str.split("/", expand=True)
-        .rename(columns={0: "month", 1: "day"})
-        .astype(int)
+df = (
+    pd.read_html(url, na_values=["-", "閉局"])[1]
+    .rename(
+        columns={
+            0: "日時",
+            1: "貯水位",
+            2: "流入量",
+            3: "放流量",
+            4: "貯水量",
+            5: "貯水率",
+        }
     )
+    .dropna(how="all", axis=1)
+)
 
-    df_date["year"] = year
+# 日時
 
-    df_time = (
-        df[1]
-        .str.split(":", expand=True)
-        .rename(columns={0: "hour", 1: "minute"})
-        .astype(int)
-    )
+df_date = df["日時"].str.extract("(\d{2}/\d{2})? *(\d{2}:\d{2})").fillna(method="ffill")
 
-    return pd.to_datetime(df_date.join(df_time))
+df_date["year"] = dt_now.year
+
+df_date[["month", "day"]] = (
+    df_date[0].str.strip().str.split("/", expand=True).astype(int)
+)
+
+df_date[["hour", "minute"]] = (
+    df_date[1].str.strip().str.split(":", expand=True).astype(int)
+)
+
+df_date["datetime"] = pd.to_datetime(
+    df_date[["year", "month", "day", "hour", "minute"]]
+)
+
+df_date["year"].mask(dt_now < df_date["datetime"], df_date["year"] - 1, inplace=True)
+
+df["日時"] = pd.to_datetime(df_date[["year", "month", "day", "hour", "minute"]])
+
+# 貯水率
+
+fig_wsr = px.line(
+    df, x="日時", y="貯水率", range_y=[40, 70], width=800, height=800, title="玉川ダムの貯水率"
+)
+fig_wsr.add_hline(y=60, line_color="orange", line_dash="dash")
+fig_wsr.add_hline(y=50, line_color="red", line_dash="dash")
+# fig_wsr.show()
+
+fig_wsr.write_image("dam.png")
+
+# 流入・放流
+
+fig_wio = px.line(
+    df,
+    x="日時",
+    y=["流入量", "放流量"],
+    range_y=[0, 10],
+    width=800,
+    height=800,
+    title="玉川ダムの流入量と放流量",
+)
+# fig_wio.show()
+
+# 表
+
+fig_wio.write_image("dio.png")
+
+fig_tbl = ff.create_table(df)
+# fig_tbl
+
+fig_tbl.write_image("table.png")
+
+# Twitter
+
+# 貯水率が欠損の行を削除
+df.dropna(subset=["貯水率"], inplace=True)
+
+df.set_index("日時", inplace=True)
+
+if len(df) > 0:
+
+    se = df.iloc[-1]
+    tw = {}
+    tw["rate"] = se["貯水率"]
+    tw["time"] = se.name.strftime("%H:%M")
+
+    twit = f'ただいまの玉川ダムの貯水率は{tw["rate"]}%です（{tw["time"]}）\n#今治 #玉川ダム #貯水率\n\nhttps://www.city.imabari.ehime.jp/suidou/suigen/dam.html'
+
+    print(twit)
 
 
-def fetch_dam(dam, dt_now):
+consumer_key = os.environ["IMABARI_CK"]
+consumer_secret = os.environ["IMABARI_CS"]
+access_token = os.environ["IMABARI_AT"]
+access_token_secret = os.environ["IMABARI_AS"]
 
-    # Twitterオブジェクトの生成
-    auth = tweepy.OAuthHandler(dam["CK"], dam["CS"])
-    auth.set_access_token(dam["AT"], dam["AS"])
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
-    api = tweepy.API(auth)
+# 複数メディア投稿
+filenames = ["table.png", "dam.png", "dio.png"]
+media_ids = []
+for filename in filenames:
+    res = api.media_upload(filename)
+    media_ids.append(res.media_id)
 
-    url = f'http://183.176.244.72/kawabou-mng/customizeMyMenuKeika.do?GID=05-5101&userId=U1001&myMenuId={dam["dam_id"]}&PG=1&KTM=3'
-
-    df = (
-        pd.read_html(url, na_values=["-", "閉局"])[1]
-        .rename(
-            columns={
-                0: "日時",
-                1: "貯水位",
-                2: "流入量",
-                3: "放流量",
-                4: "貯水量",
-                5: "貯水率",
-            }
-        )
-        .dropna(how="all", axis=1)
-    )
-
-    df["日時"] = date_parse(df["日時"], dt_now.year)
-
-    # 貯水率が欠損の行を削除
-    df.dropna(subset=["貯水率"], inplace=True)
-    
-    df.set_index("日時", inplace=True)
-
-    if len(df) > 0:
-
-        if dt_now in df.index:
-            se = df.loc[dt_now]
-        else:
-            se = df.iloc[-1]
-            
-        tw = {}
-        tw["rate"] = se["貯水率"]
-        tw["time"] = se.name.strftime("%H:%M")
-
-        twit = f'ただいまの{dam["name"]}の貯水率は{tw["rate"]}%です（{tw["time"]}）\n#今治 #{dam["name"]} #貯水率\n\nhttps://www.city.imabari.ehime.jp/suidou/suigen/dam.html'
-
-        print(twit)
-        api.update_status(twit)
-
-
-if __name__ == "__main__":
-
-    JST = datetime.timezone(datetime.timedelta(hours=+9))
-    dt_now = (datetime.datetime.now(JST) - datetime.timedelta(minutes=8)).replace(
-        minute=0, second=0, microsecond=0, tzinfo=None
-    )
-
-    fetch_dam(dam, dt_now)
+# tweet with multiple images
+api.update_status(status=twit, media_ids=media_ids)
