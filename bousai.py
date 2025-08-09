0bf06627245cc3f2c7ec24a07cccc653056d2c23
imabari <32343845+imabari@users.noreply.github.com>
2025-08-10 00:08:36 +0900

    Update bousai.py

--- bousai.py
+++ bousai.py
@@ -45,9 +45,8 @@ def fetch_dam(dt_now):
     }
 
     df = pd.json_normalize(d, record_path="min10Values").rename(columns=col).reindex(columns=col.values())
-    df["日時"] = pd.to_datetime(df["日時"])
+    df["日時"] = pd.to_datetime(df["日時"], errors="coerce")
     df.sort_values("日時", inplace=True)
-    df.set_index("日時", inplace=True)
 
     df.dropna(
         subset=[
@@ -62,7 +61,7 @@ def fetch_dam(dt_now):
     )
 
     # 貯水率
-    fig_rate = px.line(df, y="貯水率利水容量", range_y=[40, 105], width=800, height=800)
+    fig_rate = px.line(df, x="日時", y="貯水率利水容量", range_y=[40, 105], width=800, height=800)
     fig_rate.add_hline(y=65, line_color="orange", line_dash="dash")
     fig_rate.add_hline(y=50, line_color="red", line_dash="dash")
     fig_rate.update_yaxes(title="貯水率(%)")
@@ -72,14 +71,14 @@ def fetch_dam(dt_now):
     fig_rate.write_image("rate.png", width=800, height=600)
 
     # 貯水量
-    fig_vol = px.line(df, y="貯水量", range_y=[5000, 7500], width=800, height=800)
+    fig_vol = px.line(df, x="日時", y="貯水量", range_y=[5000, 7500], width=800, height=800)
     
     fig_vol.show()
 
     fig_vol.write_image("volume.png", width=800, height=600)
 
     # 流入量・放流量
-    fig_inout = px.line(df, y=["全流入量", "全放流量"], range_y=[0, 180], width=800, height=800)
+    fig_inout = px.line(df, x="日時", y=["全流入量", "全放流量"], range_y=[0, 180], width=800, height=800)
     fig_inout.update_yaxes(title=None)
 
     fig_inout.show()
@@ -108,13 +107,12 @@ def fetch_katayama(dt_now):
     }
 
     df = pd.json_normalize(d, record_path="min10Values").rename(columns=col).reindex(columns=col.values())
-    df["日時"] = pd.to_datetime(df["日時"])
+    df["日時"] = pd.to_datetime(df["日時"], errors="coerce")
     df.sort_values("日時", inplace=True)
-    df.set_index("日時", inplace=True)
 
     df.dropna(subset="水位", inplace=True)
 
-    fig = px.line(df, y="水位", width=800, height=600)
+    fig = px.line(df, x="日時", y="水位", width=800, height=600)
 
     # 氾濫注意水位
     fig.add_hline(y=2.4, line_color="orange", line_dash="dash")
